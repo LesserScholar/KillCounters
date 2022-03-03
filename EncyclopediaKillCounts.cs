e9466580faa15034f85b92a17a17fd877304fae1
Lesser Scholar <lesserscholar@gmail.com>
2022-03-03 19:03:30 +0200

    Add sort controllers for heroes and units

--- EncyclopediaKillCounts.cs
+++ EncyclopediaKillCounts.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Encyclopedia;
+using TaleWorlds.CampaignSystem.Encyclopedia.Pages;
 using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia;
 using TaleWorlds.Core.ViewModelCollection;
 using TaleWorlds.Library;
@@ -103,5 +105,79 @@ namespace KillCounters
             Kills.Add(new StringPairItemVM(new TextObject("{=0RhanD9YqlX}Bandits:").ToString(), counter.Bandits().ToString("N0")));
             Kills.Add(new StringPairItemVM(new TextObject("{=Z4RgDFHmZUn}Mercenaries:").ToString(), counter.Mercenaries().ToString("N0")));
         }
+    }
+
+    [HarmonyPatch(typeof(DefaultEncyclopediaUnitPage), "InitializeSortControllers")]
+    public class UnitPageSortingPatch {
+        static void Postfix(ref IEnumerable<EncyclopediaSortController> __result)
+        {
+            var list = __result as List<EncyclopediaSortController>;
+            if (list == null) return;
+            list.Add(
+                new EncyclopediaSortController(new TextObject("{=ltrSkEbRPv4}Kills", null), new UnitKillsComparer()));
+        }
+    }
+    public class UnitKillsComparer : DefaultEncyclopediaUnitPage.EncyclopediaListUnitComparer
+    {
+        public override int Compare(EncyclopediaListItem x, EncyclopediaListItem y)
+        {
+            return base.CompareUnits(x, y, _comparison);
+        }
+
+        public override string GetComparedValueText(EncyclopediaListItem item)
+        {
+            CharacterObject? characterObject = item.Object as CharacterObject;
+            if (characterObject != null)
+            {
+                return KillCountersBehavior.Query(characterObject.StringId).Totals().ToString();
+            }
+            Debug.FailedAssert("Unable to get the kills of a non-character object. Error in KillCounters mod.");
+            return "";
+        }
+
+        private static Func<CharacterObject, CharacterObject, int> _comparison = (CharacterObject c1, CharacterObject c2) =>
+        {
+            var k1 = KillCountersBehavior.Query(c1.StringId).Totals();
+            var k2 = KillCountersBehavior.Query(c2.StringId).Totals();
+            return k1.CompareTo(k2);
+        };
+    }
+
+
+    [HarmonyPatch(typeof(DefaultEncyclopediaHeroPage), "InitializeSortControllers")]
+    public class HeroPageSortingPatch
+    {
+        static void Postfix(ref IEnumerable<EncyclopediaSortController> __result)
+        {
+            var list = __result as List<EncyclopediaSortController>;
+            if (list == null) return;
+            list.Add(
+                new EncyclopediaSortController(new TextObject("{=ltrSkEbRPv4}Kills", null), new HeroKillsComparer()));
+        }
+    }
+    public class HeroKillsComparer : DefaultEncyclopediaHeroPage.EncyclopediaListHeroComparer
+    {
+        public override int Compare(EncyclopediaListItem x, EncyclopediaListItem y)
+        {
+            return base.CompareHeroes(x, y, _comparison);
+        }
+
+        public override string GetComparedValueText(EncyclopediaListItem item)
+        {
+            Hero? hero = item.Object as Hero;
+            if (hero != null)
+            {
+                return KillCountersBehavior.Query(hero.StringId).Totals().ToString();
+            }
+            Debug.FailedAssert("Unable to get the kills of a non-character object. Error in KillCounters mod.");
+            return "";
+        }
+
+        private static Func<Hero, Hero, int> _comparison = (Hero c1, Hero c2) =>
+        {
+            var k1 = KillCountersBehavior.Query(c1.StringId).Totals();
+            var k2 = KillCountersBehavior.Query(c2.StringId).Totals();
+            return k1.CompareTo(k2);
+        };
     }
 }
