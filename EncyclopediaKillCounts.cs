a9db91ac77e6bb0573b284bf294bb6fac2eba56b
Lesser Scholar <lesserscholar@gmail.com>
2022-03-03 18:47:31 +0200

    Add Kills to Encyclopedia Hero and Unit Pages

---
+++ EncyclopediaKillCounts.cs
@@ -0,0 +1,107 @@
+﻿using Bannerlord.UIExtenderEx.Attributes;
+using Bannerlord.UIExtenderEx.Prefabs2;
+using Bannerlord.UIExtenderEx.ViewModels;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia;
+using TaleWorlds.Core.ViewModelCollection;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+
+namespace KillCounters
+{
+    [PrefabExtension("EncyclopediaHeroPage", "descendant::Widget[@Id='InfoContainer']")]
+    public sealed class HeroPagePatch : PrefabExtensionInsertPatch
+    {
+        public override InsertType Type => InsertType.Append;
+
+        [PrefabExtensionFileName(true)]
+        public String File => "HeroPagePatch";
+
+    }
+    [PrefabExtension("EncyclopediaUnitPage", "descendant::BrushWidget/Children/Widget/Children/ListPanel/Children/Widget/Children")]
+    public sealed class UnitPagePatch : PrefabExtensionInsertPatch
+    {
+        public override InsertType Type => InsertType.Child;
+        public override int Index => 1;
+
+
+        [PrefabExtensionFileName(true)]
+        public String File => "UnitPagePatch";
+    }
+
+
+    [ViewModelMixin("RefreshValues", true)]
+    public class ExtendEncyclopediaUnitPageVM : BaseViewModelMixin<EncyclopediaUnitPageVM>
+    {
+        [DataSourceProperty]
+        public string KillsText { get; set; }
+        [DataSourceProperty]
+        public MBBindingList<StringPairItemVM> Kills { get; set; }
+
+        public ExtendEncyclopediaUnitPageVM(EncyclopediaUnitPageVM vm) : base(vm)
+        {
+            KillsText = "";
+            Kills = new MBBindingList<StringPairItemVM>();
+        }
+        public override void OnRefresh()
+        {
+            base.OnRefresh();
+
+            KillsText = new TextObject("{=ltrSkEbRPv4}Kills").ToString();
+            Kills.Clear();
+
+            var vm = base.ViewModel;
+            if (vm == null) return;
+            var character = Traverse.Create(vm).Field("_character").GetValue<CharacterObject>();
+            if (character == null) return;
+
+            var counter = KillCountersBehavior.Query(character.StringId);
+
+            Kills.Add(new StringPairItemVM(new TextObject("{=6nVL5daR87t}Regular Troops:").ToString(), counter.RegularTroops().ToString("N0")));
+            Kills.Add(new StringPairItemVM(new TextObject("{=5A34EzKTyLT}Heroes:").ToString(), counter.Heroes().ToString("N0")));
+            Kills.Add(new StringPairItemVM(new TextObject("{=0RhanD9YqlX}Bandits:").ToString(), counter.Bandits().ToString("N0")));
+            Kills.Add(new StringPairItemVM(new TextObject("{=Z4RgDFHmZUn}Mercenaries:").ToString(), counter.Mercenaries().ToString("N0")));
+        }
+    }
+
+
+    [ViewModelMixin("RefreshValues", true)]
+    public class ExtendEncyclopediaHeroPageVM : BaseViewModelMixin<EncyclopediaHeroPageVM>
+    {
+        [DataSourceProperty]
+        public string KillsText { get; set; }
+        [DataSourceProperty]
+        public MBBindingList<StringPairItemVM> Kills { get; set; }
+
+        public ExtendEncyclopediaHeroPageVM(EncyclopediaHeroPageVM vm) : base(vm)
+        {
+            KillsText = "";
+            Kills = new MBBindingList<StringPairItemVM>();
+        }
+        public override void OnRefresh()
+        {
+            base.OnRefresh();
+
+            KillsText = new TextObject("{=ltrSkEbRPv4}Kills").ToString();
+            Kills.Clear();
+
+            var vm = base.ViewModel;
+            if (vm == null) return;
+            var hero = Traverse.Create(vm).Field("_hero").GetValue<Hero>();
+            if (hero == null) return;
+
+            var counter = KillCountersBehavior.Query(hero.StringId);
+
+            Kills.Add(new StringPairItemVM(new TextObject("{=6nVL5daR87t}Regular Troops:").ToString(), counter.RegularTroops().ToString("N0")));
+            Kills.Add(new StringPairItemVM(new TextObject("{=5A34EzKTyLT}Heroes:").ToString(), counter.Heroes().ToString("N0")));
+            Kills.Add(new StringPairItemVM(new TextObject("{=0RhanD9YqlX}Bandits:").ToString(), counter.Bandits().ToString("N0")));
+            Kills.Add(new StringPairItemVM(new TextObject("{=Z4RgDFHmZUn}Mercenaries:").ToString(), counter.Mercenaries().ToString("N0")));
+        }
+    }
+}
--- SubModule.cs
+++ SubModule.cs
@@ -1,21 +1,24 @@
-﻿using HarmonyLib;
-using System.Collections.Generic;
+﻿using Bannerlord.UIExtenderEx;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.ObjectSystem;
-using TaleWorlds.SaveSystem;
 
 namespace KillCounters
 {
     public class SubModule : MBSubModuleBase
     {
         Harmony _harmony = new Harmony("KillCounters");
+        private UIExtender _extender = new UIExtender("KillCounters");
+
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
 
             _harmony.PatchAll();
+
+            _extender.Register(typeof(SubModule).Assembly);
+            _extender.Enable();
         }
 
         protected override void OnSubModuleUnloaded()
@@ -40,151 +43,4 @@ namespace KillCounters
         }
     }
 
-    internal class KillCounterTypeDefiner : SaveableTypeDefiner
-    {
-        public KillCounterTypeDefiner() : base(852_768_580) { }
-
-        protected override void DefineClassTypes()
-        {
-            AddClassDefinition(typeof(KillCounter), 1);   
-        }
-
-        protected override void DefineContainerDefinitions()
-        {
-            ConstructContainerDefinition(typeof(Dictionary<string, KillCounter>));
-        }
-    }
-
-    public class KillCounter {
-
-        [SaveableField(1)]
-        Dictionary<string, int> counters = new Dictionary<string, int>();
-
-        [SaveableField(2)]
-        int total = 0;
-
-        public void AddKill(string id)
-        {
-            total++;
-            if (counters.ContainsKey(id))
-            {
-                counters[id]++;
-            } else
-            {
-                counters[id] = 1;
-            }
-        }
-
-        public int Totals()
-        {
-            return total;
-        }
-        public int Bandits()
-        {
-            int result = 0;
-            foreach (var c in counters)
-            {
-                CharacterObject characterObject = MBObjectManager.Instance.GetObject<CharacterObject>(c.Key);
-                if (characterObject.Occupation == Occupation.Bandit)
-                {
-                    result += c.Value;
-                }
-            }
-            return result;
-        }
-        public int Mercenaries()
-        {
-            int result = 0;
-            foreach (var c in counters)
-            {
-                CharacterObject characterObject = MBObjectManager.Instance.GetObject<CharacterObject>(c.Key);
-                if (characterObject.Occupation == Occupation.Mercenary)
-                {
-                    result += c.Value;
-                }
-            }
-            return result;
-        }
-        public int Heroes()
-        {
-            int result = 0;
-            foreach (var c in counters)
-            {
-                CharacterObject characterObject = MBObjectManager.Instance.GetObject<CharacterObject>(c.Key);
-                if (characterObject.IsHero)
-                {
-                    result += c.Value;
-                }
-            }
-            return result;
-        }
-        public int RegularTroops()
-        {
-            int result = 0;
-            foreach (var c in counters)
-            {
-                CharacterObject characterObject = MBObjectManager.Instance.GetObject<CharacterObject>(c.Key);
-                if (!characterObject.IsHero && characterObject.Occupation != Occupation.Mercenary && characterObject.Occupation != Occupation.Bandit)
-                {
-                    result += c.Value;
-                }
-            }
-            return result;
-        }
-    }
-
-    public class KillCountersBehavior : CampaignBehaviorBase
-    {
-        public KillCountersBehavior() {
-            _counters = new Dictionary<string, KillCounter>();
-        }
-
-        public void AddKill(BasicCharacterObject strikerTroop, BasicCharacterObject attackedTroop)
-        {
-            var id = strikerTroop.StringId;
-            if (!_counters.ContainsKey(id))
-            {
-                _counters[id] = new KillCounter();
-            }
-            _counters[id].AddKill(attackedTroop.StringId);
-        }
-
-        public override void RegisterEvents()
-        {
-        }
-
-        //Dictionary<BasicCharacterObject, Tracker> _trackers;
-        Dictionary<string, KillCounter> _counters;
-
-        public override void SyncData(IDataStore dataStore)
-        {
-            dataStore.SyncData("_counters", ref _counters);
-        }
-    }
-
-    [HarmonyPatch(typeof(MapEventSide), nameof(MapEventSide.ApplySimulatedHitRewardToSelectedTroop))]
-    class MapEventSidePatch : HarmonyPatch
-    {
-        static void Postfix(CharacterObject strikerTroop, CharacterObject attackedTroop, bool isFinishingStrike)
-        {
-            if (!isFinishingStrike) return;
-
-            var counters = Campaign.Current.GetCampaignBehavior<KillCountersBehavior>();
-
-            counters.AddKill(strikerTroop, attackedTroop);
-        }
-    }
-
-    [HarmonyPatch(typeof(Mission), "OnAgentRemoved")]
-    class MissionPatch : HarmonyPatch
-    {
-        static void Postfix(Agent affectedAgent, Agent affectorAgent)
-        {
-            if (affectorAgent != null)
-            {
-                var counters = Campaign.Current.GetCampaignBehavior<KillCountersBehavior>();
-                counters.AddKill(affectorAgent.Character, affectedAgent.Character);
-            }
-        }
-    }
 }
